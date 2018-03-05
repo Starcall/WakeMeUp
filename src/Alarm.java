f27c514c9f2c993110d7ed961df1ecd6bf316ee8
starcall <ineedletter@gmail.com>
2018-03-05 21:11:56 +0300

    Alarm class

--- src/Alarm.java
+++ src/Alarm.java
@@ -1,3 +1,13 @@
 public class Alarm {
+    private String eventDayOfTheWeek;
+    private String eventTimeOfTheDay;
 
+    public Alarm(String eventDayOfTheWeek, String eventTimeOfTheDay) {
+        this.eventDayOfTheWeek = eventDayOfTheWeek;
+        this.eventTimeOfTheDay = eventTimeOfTheDay;
+    }
+
+    public String getEvent() {
+        return eventDayOfTheWeek + " " + eventTimeOfTheDay;
+    }
 }
